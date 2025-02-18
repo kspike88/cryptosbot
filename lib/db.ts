c4798b1155d28240db647f07b4e999a5c6764cbd
gleb <black.spike881@gmail.com>
2025-02-18 10:31:58 +0200

    logdbts111

--- lib/db.ts
+++ lib/db.ts
@@ -1,6 +1,8 @@
 import { Pool } from 'pg';
 
-// Создаем и экспортируем пул подключений
+// Временный лог для проверки
+console.log('DATABASE_URL:', process.env.DATABASE_URL);
+
 export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
